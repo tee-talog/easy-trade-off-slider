f2a88ad49ba597b4886b06634917eeef6f0cfed0
talog <tee.talog@gmail.com>
2022-07-18 23:03:09 +0900

    unmemorize callback functions

--- components/Rows.tsx
+++ components/Rows.tsx
@@ -19,35 +19,14 @@ const Rows = ({
   onDecrementWithIndex,
   onValueChangeWithIndex,
 }: Props) => {
-  const memoOnIncrementWithIndex = (index: number) =>
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useCallback(() => {
-      onIncrementWithIndex(index)
-    }, [index])
-
-  const memoOnDecrementWithIndex = (index: number) =>
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useCallback(() => {
-      onDecrementWithIndex(index)
-    }, [index])
-
-  const memoOnValueChangeWithIndex = (index: number) =>
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useCallback(
-      (value: number) => {
-        onValueChangeWithIndex(index)(value)
-      },
-      [index]
-    )
-
   const domItems = items.map(({ label, value }, i) => (
     <Row
       key={i}
       label={label}
       value={value}
-      onIncrement={memoOnIncrementWithIndex(i)}
-      onDecrement={memoOnDecrementWithIndex(i)}
-      onValueChange={memoOnValueChangeWithIndex(i)}
+      onIncrement={() => onIncrementWithIndex(i)}
+      onDecrement={() => onDecrementWithIndex(i)}
+      onValueChange={() => onValueChangeWithIndex(i)}
     />
   ))
 
